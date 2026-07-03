f877dd08d59c2f0144002bd71595c813f2eec81f
bcroner <seekandwander@gmail.com>
2026-07-03 11:12:44 -0700

    mofo

--- UniverseComputerSimulation.cpp
+++ UniverseComputerSimulation.cpp
@@ -38,7 +38,7 @@ void simp_point_vector_append(Point*** v, __int64* vtop, __int64* vcap, Point* d
 
 }
 
-Point* QuantumUniverseComputerSimulation_create_point(__int64 x, __int64 y, __int64 z, energy_state point_state) {
+Point* PointUniverseComputerSimulation_create_point(__int64 x, __int64 y, __int64 z, energy_state point_state) {
 
     Point* point = new Point();
 
@@ -48,6 +48,104 @@ Point* QuantumUniverseComputerSimulation_create_point(__int64 x, __int64 y, __in
     point->point_state = point_state;
 
 	return point;
+}
+
+
+__int64* simp_vector_create(__int64 init_sz) {
+
+    __int64* ret = new __int64[init_sz];
+    return ret;
+
+}
+
+__int64 simp_vector_read(__int64* v, __int64 vtop, __int64 vcap, __int64 loc) {
+
+    if (loc > vtop)
+        return 0;
+
+    return v[loc];
+}
+
+void simp_vector_append(__int64** v, __int64* vtop, __int64* vcap, __int64 data) {
+
+    *vtop = *vtop + 1;
+
+    if (*vtop < *vcap)
+        (*v)[*vtop] = data;
+    else {
+        __int64* newv = new __int64[*vcap * 2];
+        for (__int64 i = 0; i < *vcap * 2; i++)
+            newv[i] = 0;
+        for (__int64 i = 0; i < *vcap; i++)
+            newv[i] = (*v)[i];
+        *vcap = *vcap * 2;
+        delete[](*v);
+        *v = newv;
+        (*v)[*vtop] = data;
+    }
+
+}
+
+
+int id_pool_retrieve(__int64* id_pool, __int64* id_pool_vtop, __int64* id_pool_vcap) {
+
+    if (*id_pool_vtop == -1) {
+        simp_vector_append(*id_pool, id_pool_vtop, id_pool_vcap, 0);
+        return 0;
+    }
+
+    __int64 id = id_pool[*id_pool_vtop + 1];
+
+    __int64 ix = -1;
+
+    for (__int64 i = 0; i <= *id_pool_vtop + 1; i++) {
+        if (id_pool[i] > id) {
+            ix = i;
+            break;
+        }
+    }
+
+    for (__int64 i = ix; i <= *id_pool_vtop; i++)
+        id_pool[i] = id_pool[i + 1];
+
+    return id;
+
+}
+
+void id_pool_submit(__int64* id_pool, __int64* id_pool_vtop, __int64* id_pool_vcap, __int64 id) {
+
+    __int64 ix = -1;
+
+    for (__int64 i = 0; i <= *id_pool_vtop + 1; i++) {
+        if (id_pool[i] > id) {
+            ix = i;
+            break;
+        }
+    }
+
+    simp_vector_append(&(id_pool), id_pool_vtop, id_pool_vcap, id_pool[*id_pool_vtop + 1]);
+
+    for (__int64 i = ix; i <= *id_pool_vtop; i++)
+        id_pool[i] = id_pool[i + 1];
+
+    id_pool[ix] = id;
+
+}
+
+
+ID_Pool* create_id_pool() {
+
+    ID_Pool* id_pool = new ID_Pool();
+
+    id_pool->simulation_entities = simp_vector_create(16);
+
+    id_pool->simulation_entities_vtop = -1;
+    id_pool->simulation_entities_vcap = 16;
+
+    id_pool_retrieve(id_pool->simulation_entities, &(id_pool->simulation_entities_vtop), &(id_pool->simulation_entities_vcap));
+    id_pool_retrieve(id_pool->simulation_entities, &(id_pool->simulation_entities_vtop), &(id_pool->simulation_entities_vcap));
+
+    return id_pool;
 }
 
 #endif
--- UniverseComputerSimulation.hpp
+++ UniverseComputerSimulation.hpp
@@ -21,6 +21,15 @@ typedef struct Point_tag {
 
 } Point;
 
+typedef struct ID_Pool_tag {
+
+	__int64* simulation_entities;
+
+	__int64 simulation_entities_vtop;
+	__int64 simulation_entities_vcap;
+
+} ID_Pool;
+
 typedef struct Atom_Star_Galaxy_Dark_Matter_tag {
 
 	__int64 x;
@@ -75,5 +84,13 @@ Point* simp_point_vector_create(__int64 init_sz);
 Point* simp_point_vector_read(Point** v, __int64 vtop, __int64 vcap, __int64 loc);
 void simp_point_vector_append(Point*** v, __int64* vtop, __int64* vcap, Point* data);
 Point* PointUniverseComputerSimulation_create_point(__int64 x, __int64 y, __int64 z, energy_state point_state);
+__int64* simp_vector_create(__int64 init_sz);
+__int64 simp_vector_read(__int64* v, __int64 vtop, __int64 vcap, __int64 loc);
+void simp_vector_append(__int64** v, __int64* vtop, __int64* vcap, __int64 data);
+int id_pool_retrieve(__int64* id_pool, __int64* id_pool_vtop, __int64* id_pool_vcap);
+void id_pool_submit(__int64* id_pool, __int64* id_pool_vtop, __int64* id_pool_vcap, __int64 id);
+int id_pool_retrieve(__int64* id_pool, __int64* id_pool_vtop, __int64* id_pool_vcap);
+void id_pool_submit(__int64* id_pool, __int64* id_pool_vtop, __int64* id_pool_vcap, __int64 id);
+ID_Pool* create_id_pool();
 
 #endif
